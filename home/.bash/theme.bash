386a2fd61a3e2741f13b2ae7fa1e1bfe5113e5b1
Simon Eskildsen <sirup@sirupsen.com>
2013-11-05 21:23:34 -0500

    Fix ps1

--- home/.bash/theme.bash
+++ home/.bash/theme.bash
@@ -1,27 +1,16 @@
 #!/bin/bash
 
 # Start from scratch
-PS1=''
-
-# Set hostname if not on MacBook (home) and not in tmux (where hostname is shown
-# right in the prompt).
-if [[ ! $(hostname) =~ MacBook && -z $TMUX ]]; then
-  PS1="\[$ORANGE\]\h "
-fi
-
-# Put in current directory only
-PS1+="\[$RED\]\W"
 
 # OS X Homebrew
 source /usr/local/etc/bash_completion.d/git-prompt.sh 2> /dev/null
 # Ubuntu Linux
 source /etc/bash_completion.d/git 2> /dev/null
 
-# If we have __git_ps1 installed, then put it in the prompt. We do what we can
-# from the previous two lines.
-if command -v __git_ps1 > /dev/null 2>&1; then
-  PS1+="\[$YELLOW\]$(__git_ps1 " (%s)")"
+# Set hostname if not on MacBook (home) and not in tmux (where hostname is shown
+# right in the prompt).
+if [[ ! $(hostname) =~ MacBook && -z $TMUX ]]; then
+  PS1="\[$ORANGE\]\h \[$RED\]\W \[$YELLOW\]$(__git_ps1 " (%s)")\[${NORMAL}\]$ "
+else
+  PS1="\[$RED\]\W \[$YELLOW\]$(__git_ps1 " (%s)")\[${NORMAL}\]$ "
 fi
-
-# Normalize prompt contents
-PS1+="\[${NORMAL}\] $ "
