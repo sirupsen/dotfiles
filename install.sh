12cd8cdffcc71fe49d4eb5837e1da1428afc49bd
Simon Eskildsen <sirup@sirupsen.com>
2013-12-06 17:17:33 +0000

    install.sh: support downloading binaries

--- install.sh
+++ install.sh
@@ -10,36 +10,46 @@ CHRUBY_VERSION="0.3.7"
 VIM_VERSION="7.4"
 
 cores() {
-  4
+  if [[ `uname` == 'Darwin' ]]; then
+    `sysctl -n hw.ncpu`
+  else
+    `nproc`
+  fi
 }
 
-# $1: Ruby version (ruby-2.0.0-p247
-# $2: Ruby git sha for version (v2_0_0_247)
 install_mri_ruby() {
   if [[ ! -d "$HOME/.rubies/$1" ]]; then
-    echo "Installing packages required for $1"
+    echo "Installing packages required for $1..."
 
-    if [[ $(uname) = 'Linux' ]]; then
+    export platform=`uname | tr '[:upper:]' '[:lower:]'`
+
+    if [[ $platform = 'linux' ]]; then
       sudo apt-get -y update
       sudo apt-get -y install build-essential zlib1g-dev libssl-dev\
         libreadline6-dev libyaml-dev libxslt-dev libxml2-dev
     fi
 
-    echo "Installing $1"
+    echo "Installing $1..."
 
     (
-     cd $HOME/.rubies/ruby-trunk
-     git checkout $2
-     git clean -f
-     autoconf
-     ./configure --prefix "$HOME/.rubies/$1" --disable-install-doc
-     make -j"$(cores)"
-     make install
-     git checkout trunk
+      cd /tmp
 
-     source ~/.bashrc
-     chruby $1
-     gem install bundler
+      mkdir -p ~/.rubies
+
+      if wget "http://shopify-vagrant.s3.amazonaws.com/rubies/$1-$platform.tar.gz"; then
+        tar zxf "$1-$platform.tar.gz"
+        mv /tmp/$1 ~/.rubies/
+      else
+        wget http://cache.ruby-lang.org/pub/ruby/$1.tar.gz
+        tar zxf $1.tar.gz
+        cd $1
+
+        ./configure --prefix "$HOME/.rubies/$1" --disable-install-doc
+        make -j"$(cores)"
+        make install
+      fi
+
+      rm -rf /tmp/ruby-*
     )
   fi
 }
@@ -47,13 +57,13 @@ install_mri_ruby() {
 ./linker.sh
 
 if [[ ! -d $HOME/.vim/bundle/neobundle.vim ]]; then
-  echo "Installing Neobundle"
+  echo "Installing neobundle..."
   git clone git://github.com/Shougo/neobundle.vim $HOME/.vim/bundle/neobundle.vim
   vim +NeoBundleInstall +qall
 fi
 
 if ! type -t chruby > /dev/null 2>&1; then
-  echo "Installing chruby"
+  echo "Installing chruby..."
 
   (
     cd /tmp
@@ -64,35 +74,35 @@ if ! type -t chruby > /dev/null 2>&1; then
   )
 fi
 
-if [[ ! -d $HOME/.rubies/ruby-trunk ]]; then
-  git clone https://github.com/ruby/ruby.git ~/.rubies/ruby-trunk
-fi
+install_mri_ruby "ruby-2.0.0-p353"
+install_mri_ruby "ruby-1.9.3-p484"
 
-install_mri_ruby "ruby-2.0.0-p353" "v2_0_0_353"
+# Refresh chruby
+source ~/.bashrc
 
 if ! vim --version | grep -q "+ruby"; then
-  echo "Installing Vim 7.4 with Ruby support"
+  echo "Installing Vim 7.4 with Ruby support..."
   (
     cd /tmp
     wget "ftp://ftp.vim.org/pub/vim/unix/vim-$VIM_VERSION.tar.bz2"
     tar xjf "vim-$VIM_VERSION.tar.bz2"
     cd vim74
+    chruby 2.0.0
     ruby --version
     ./configure --enable-rubyinterp
     make -j"$(cores)"
     sudo make install
   )
-  
+
   # Refresh links
   hash -r
 fi
 
-if [[ ! -f $HOME/.vim/bundle/Command-T/ruby/command-t/match.o ]]; then
-  echo "Compiling commandt"
-  (
-    cd $HOME/.vim/bundle/Command-T/ruby/command-t
-    ruby extconf.rb
-    make clean
-    make
-  )
-fi
+echo "Compiling commandt..."
+(
+  cd $HOME/.vim/bundle/Command-T/ruby/command-t
+  chruby 2.0.0
+  ruby extconf.rb
+  make clean
+  make
+)
