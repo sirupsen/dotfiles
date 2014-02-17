67de4b2a3f10818acff0dd3ab8b501ffc29bc5cc
Simon Eskildsen <sirup@sirupsen.com>
2014-02-17 18:01:06 +0000

    install.sh: ruby-install

--- install.sh
+++ install.sh
@@ -19,43 +19,6 @@ cores() {
   fi
 }
 
-install_mri_ruby() {
-  if [[ ! -d "$HOME/.rubies/$1" ]]; then
-    echo "Installing packages required for $1..."
-
-    export platform=`uname | tr '[:upper:]' '[:lower:]'`
-
-    if [[ $platform = 'linux' ]]; then
-      sudo apt-get -y update
-      sudo apt-get -y install build-essential zlib1g-dev libssl-dev\
-        libreadline6-dev libyaml-dev libxslt-dev libxml2-dev
-    fi
-
-    echo "Installing $1..."
-
-    (
-      cd /tmp
-
-      mkdir -p ~/.rubies
-
-      if wget "http://shopify-vagrant.s3.amazonaws.com/rubies/$1-$platform.tar.gz"; then
-        tar zxf "$1-$platform.tar.gz"
-        mv /tmp/$1 ~/.rubies/
-      else
-        wget http://cache.ruby-lang.org/pub/ruby/$1.tar.gz
-        tar zxf $1.tar.gz
-        cd $1
-
-        ./configure --prefix "$HOME/.rubies/$1" --disable-install-doc
-        make -j"$(cores)"
-        make install
-      fi
-
-      rm -rf /tmp/ruby-*
-    )
-  fi
-}
-
 ./linker.sh
 
 if [[ ! -d $HOME/.vim/bundle/neobundle.vim ]]; then
@@ -65,8 +28,6 @@ if [[ ! -d $HOME/.vim/bundle/neobundle.vim ]]; then
 fi
 
 if ! type -t chruby > /dev/null 2>&1; then
-  echo "Installing chruby..."
-
   (
     cd /tmp
     wget -O "chruby-$CHRUBY_VERSION.tar.gz" "https://github.com/postmodern/chruby/archive/v$CHRUBY_VERSION.tar.gz"
@@ -76,20 +37,26 @@ if ! type -t chruby > /dev/null 2>&1; then
   )
 fi
 
-#install_mri_ruby "ruby-2.0.0-p353"
-#install_mri_ruby "ruby-1.9.3-p484"
+if ! type -t ruby-install > /dev/null 2>&1; then
+  (
+    cd /tmp
+    wget -O ruby-install-0.3.4.tar.gz https://github.com/postmodern/ruby-install/archive/v0.3.4.tar.gz
+    tar -xzvf ruby-install-0.3.4.tar.gz
+    cd ruby-install-0.3.4/
+    sudo make install
+  )
+fi
 
 if ! vim --version | grep -q "+ruby"; then
   echo "Installing Vim 7.4 with Ruby support..."
   (
     cd /tmp
     source /usr/local/share/chruby/chruby.sh 
-    ruby -v
+    ruby --version
 
     wget "ftp://ftp.vim.org/pub/vim/unix/vim-$VIM_VERSION.tar.bz2"
     tar xjf "vim-$VIM_VERSION.tar.bz2"
     cd vim74
-    ruby --version
     ./configure --enable-rubyinterp
     make -j"$(cores)"
     sudo make install
