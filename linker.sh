ad09fccfb9cf5de2144e0768f492ce9b596bd0f5
Simon Eskildsen <sirup@sirupsen.com>
2014-02-19 18:31:00 +0000

    linker: add colors

--- linker.sh
+++ linker.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Links everything in home/ to ~/, does sanity checks.
+# By Simon Eskildsen (github.com/Sirupsen)
 
 function symlink {
   ln -nsf $1 $2
@@ -10,18 +12,18 @@ for file in home/.[^.]*; do
   target="$HOME/$(basename $file)"
 
   if [[ -h $target && ($(readlink $target) == $path)]]; then
-    echo "~/$base is symlinked to your dotfiles."
+    echo -e "\e[90m~/$base is symlinked to your dotfiles.\e[39m"
   elif [[ -f $target && $(md5 $path) == $(md5 $target) ]]; then
-    echo "~/$base exists and was identical to your dotfile. Overriding with symlink."
+    echo -e "\e[32m~/$base exists and was identical to your dotfile. Overriding with symlink.\e[39m"
     symlink $path $target
   elif [[ -a $target ]]; then
-    read -p "~/$base exists and differs from your dotfile. Override? [yn]" -n 1
+    read -p "\e[33m~/$base exists and differs from your dotfile. Override?  [yn]\e[39m" -n 1
 
     if [[ $REPLY =~ [yY]* ]]; then
       symlink $path $target
     fi
   else
-    echo "~/$base does not exist. Symlinking to dotfile."
+    echo -e "\e[32m~/$base does not exist. Symlinking to dotfile.\e[39m"
     symlink $path $target
   fi
 done
