e44ed2955c84a91e5ebe5c975c257b4c7505c62a
Simon Eskildsen <sirup@sirupsen.com>
2014-02-21 10:12:28 -0500

    tmux: dont use rename_tmux_window when not in tmux
    cc @teoljungberg

--- home/.bash/tmux.bash
+++ home/.bash/tmux.bash
@@ -1,6 +1,6 @@
 rename_tmux_window_to_current_dir() {
-  if [ "$TERM" = "screen-256color" ]; then
-    if [ "$PWD" != "$LPWD" ]; then
+  if [[ "$TERM" == "screen-256color" && -n "$TMUX" ]]; then
+    if [[ "$PWD" != "$LPWD" ]]; then
       LPWD="$PWD"
       tmux rename-window ${PWD//*\//}
     fi
