6b229e38166db8229f9f3d40a43ffa070a8225ff
Simon Eskildsen <sirup@sirupsen.com>
2014-02-22 21:08:04 -0500

    linker: use standard escaping

--- linker.sh
+++ linker.sh
@@ -12,18 +12,18 @@ for file in home/.[^.]*; do
   target="$HOME/$(basename $file)"
 
   if [[ -h $target && ($(readlink $target) == $path)]]; then
-    echo -e "\e[90m~/$base is symlinked to your dotfiles.\e[39m"
+    echo -e "\x1B[90m~/$base is symlinked to your dotfiles.\x1B[39m"
   elif [[ -f $target && $(md5 $path) == $(md5 $target) ]]; then
-    echo -e "\e[32m~/$base exists and was identical to your dotfile. Overriding with symlink.\e[39m"
+    echo -e "\x1B[32m~/$base exists and was identical to your dotfile.  Overriding with symlink.\x1B[39m"
     symlink $path $target
   elif [[ -a $target ]]; then
-    read -p "\e[33m~/$base exists and differs from your dotfile. Override?  [yn]\e[39m" -n 1
+    read -p "\x1B[33m~/$base exists and differs from your dotfile. Override?  [yn]\x1B[39m" -n 1
 
     if [[ $REPLY =~ [yY]* ]]; then
       symlink $path $target
     fi
   else
-    echo -e "\e[32m~/$base does not exist. Symlinking to dotfile.\e[39m"
+    echo -e "\x1B[32m~/$base does not exist. Symlinking to dotfile.\x1B[39m"
     symlink $path $target
   fi
 done
