d6691da54b7e1c6e96c85754ea9740d503eebfb9
Simon Eskildsen <sirup@sirupsen.com>
2014-02-24 13:28:31 +0000

    linux: remove bash completion

--- home/.bash/02_completion.bash
+++ home/.bash/02_completion.bash
@@ -6,8 +6,8 @@ if [[ -d /usr/local/etc/bash_completion.d ]]; then
   done
 fi
 
-if [[ -d /etc/bash_completion.d ]]; then
-  for file in /etc/bash_completion.d/*; do
-    source $file
-  done
-fi
+#if [[ -d /etc/bash_completion.d ]]; then
+#  for file in /etc/bash_completion.d/*; do
+#    source $file
+#  done
+#fi
