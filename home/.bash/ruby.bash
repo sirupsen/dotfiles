3fbdf470d0fd3fb28bf0f642c7e122f529369480
Simon Eskildsen <sirup@sirupsen.com>
2015-01-25 18:22:30 +0000

    ruby: use latest non trunk

--- home/.bash/ruby.bash
+++ home/.bash/ruby.bash
@@ -1,19 +1,11 @@
 #!/bin/bash
 
-# <= 2.0
-# export RUBY_HEAP_MIN_SLOTS=1000000
-# export RUBY_HEAP_SLOTS_INCREMENT=500000
-# export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
-# export RUBY_GC_MALLOC_LIMIT=99000000
-# export RUBY_HEAP_FREE_MIN=100000
-# export RUBYOPT=""
-
 if [[ -r /usr/local/share/chruby/chruby.sh && -z $SHOPIFY_DEV_VAGRANT ]]; then
   source /usr/local/share/chruby/chruby.sh
   RUBIES=(~/.rubies/*)
 
-  chruby 2.1.3
+  # switch to latest stable
+  chruby $(chruby | grep -v trunk | tail -n1 | awk '{ print $2 }')
 
-  # Allow auto-switching in directories with a .ruby-version # file
   source /usr/local/share/chruby/auto.sh
 fi
