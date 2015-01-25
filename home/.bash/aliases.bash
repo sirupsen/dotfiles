cbc37893382793729c907c5ec27a830287a14833
Simon Eskildsen <sirup@sirupsen.com>
2015-01-25 18:17:43 +0000

    alias: remove unused

--- home/.bash/aliases.bash
+++ home/.bash/aliases.bash
@@ -1,41 +1,5 @@
 #! /bin/bash
 
-# This is for smart running of tests. If .zeus.sock exists, then we run the
-# tests with Zeus, otherwise, fall back to Ruby.
-function rt {
-  if [ -e .zeus.sock ]; then
-    bundle exec zeus test $1
-  # elif grep -q "spring-commands-testunit" Gemfile; then
-  #   bundle exec spring testunit $@
-  else
-    bundle exec ruby -Itest $1
-  fi
-}
-
-# Paste code via stdin to eval.in. It relies on the language auto-detecting
-# feature.
-function evalin {
-  CODE=`cat`
-  curl -s --dump-header - --data "code=$CODE&execute=true" https://eval.in/ | grep -woE "https:\/\/eval.in/[0-9]+"
-}
-
-function knife-each {
-  knife search -i node $1 | tail -n +3
-}
-
-function knife-each-ssh {
-  knife-each $1 | xargs -I^ $3 ssh simon@^ "hostname && $2"
-}
-
-function host-each-ssh {
-  IFS=','; for host in $HOST; do xargs -I^ $3 ssh simon@^ "hostname && $2"; done
-}
-
-function dotfiles {
-  cd ~/.dotfiles
-  git pull origin master
-}
-
 if command -v hub > /dev/null; then
   alias git=hub
 fi
@@ -68,7 +32,6 @@ git-find-merge() {
     | tail -1
 }
 
-
 alias gp='git push `git_origin_or_fork` `cbranch`'
 alias gpl='git pull `git_origin_or_fork` `cbranch`'
 alias gc='git commit --verbose'
@@ -82,15 +45,12 @@ if type -t __git_complete > /dev/null; then
 fi
 alias gpf='if [[ $(cbranch) != "master" ]]; then git push `git_origin_or_fork` +`cbranch`; else echo "Not going to force push master bud"; fi'
 alias gd='git diff'
+alias gg='git grep'
 alias gupdate='git fetch origin && git rebase origin/master && gpf'
 alias blush="git commit --amend --reuse-message HEAD"
 
 alias bx='bundle exec'
-alias bxr='bundle exec rake'
-
-alias vu='vagrant up'
-alias vsu='vagrant suspend'
-alias vr='vagrant resume'
+alias rt='bx ruby -Itest'
 alias vs='vagrant ssh'
 
 vss() {
@@ -102,20 +62,3 @@ vss() {
 alias ttc='tmux save-buffer -|pbcopy'
 alias tfc='tmux set-buffer "$(pbpaste)"'
 
-alias gg='git grep'
-
-alias open-ports="sudo lsof -iTCP -sTCP:LISTEN -P"
-alias walrus="ruby -e 'loop { 0.upto(50) { |i| print \"\r\" + (\" \" * i) + \":\" + %w(€ c)[i%2] + \".\" * (50-i); sleep 0.25 } }'"
-
-green() {
-  walrus & >/dev/null 2>&1
-  WALRUS_PID=$!
-
-  while true; do
-    git ci-status > /dev/null \
-      && say "$(basename `pwd`) green" \
-      && kill -SIGKILL $WALRUS_PID > /dev/null 2>&1 \
-      && return
-    sleep 0.1
-  done
-}
