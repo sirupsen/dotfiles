c0dfb434523b979b94aee111096274317f70c80d
Simon Eskildsen <sirup@sirupsen.com>
2015-02-06 17:56:53 -0500

    ruby: fix setting ruby version

--- home/.bash/ruby.bash
+++ home/.bash/ruby.bash
@@ -5,7 +5,7 @@ if [[ -r /usr/local/share/chruby/chruby.sh && -z $SHOPIFY_DEV_VAGRANT ]]; then
   RUBIES=(~/.rubies/*)
 
   # switch to latest stable
-  chruby $(chruby | grep -v trunk | tail -n1 | awk '{ print $2 }')
+  chruby $(chruby | grep -v trunk | tail -n1 | awk '{print $1}')
 
   source /usr/local/share/chruby/auto.sh
 fi
