6a5dd9f7035f9c41a2b6c246738d5229556bd1b9
Simon Eskildsen <sirup@sirupsen.com>
2015-03-06 18:13:10 +0000

    tmux: rename ssh windows to target

--- home/.bash/tmux.bash
+++ home/.bash/tmux.bash
@@ -7,4 +7,23 @@ rename_tmux_window_to_current_dir() {
   fi
 }
 
+ssh() {
+    if [[ -z $TMUX ]]; then
+        /usr/bin/ssh $@
+        return
+    fi
+
+    local remote=${@: -1}
+    local old_name=$(tmux display-message -p '#W')
+
+    if [[ -n $remote ]]; then
+        tmux rename-window "$remote"
+    fi
+
+    /usr/bin/ssh $@
+
+    tmux rename-window "$old_name"
+}
+
 export PROMPT_COMMAND=rename_tmux_window_to_current_dir
+
