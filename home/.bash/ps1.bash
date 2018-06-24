7ebe56f23051057ebc145747dde27dc87f10f438
Simon Eskildsen <sirup@sirupsen.com>
2018-06-24 07:39:49 -0400

    ps1: newline in small terms

--- home/.bash/ps1.bash
+++ home/.bash/ps1.bash
@@ -1,26 +1,35 @@
 #!/bin/bash
 
-PS1=''
+build_ps1() {
+  PS1=''
 
-# Put in current directory only
-PS1+='\[$RED\]\W'
+  # Put in current directory only
+  PS1+='\[$RED\]\W'
 
-# If we have __git_ps1 installed, then put it in the prompt. We do what we can
-# from the previous two lines.
-if command -v __git_ps1 > /dev/null 2>&1; then
-  PS1+='\[$YELLOW\]$(__git_ps1 " (%s)")'
-fi
+  # If we have __git_ps1 installed, then put it in the prompt. We do what we can
+  # from the previous two lines.
+  if command -v __git_ps1 > /dev/null 2>&1; then
+    PS1+='\[$YELLOW\]$(__git_ps1 " (%s)")'
+  fi
 
-# https://github.com/jonmosco/kube-ps1
-export KUBE_PS1_SYMBOL_ENABLE=false
-export KUBE_PS1_PREFIX=''
-export KUBE_PS1_SUFFIX=''
-export KUBE_PS1_DIVIDER='/'
-export KUBE_PS1_CTX_COLOR='magenta'
-export KUBE_PS1_NS_COLOR='gray'
-if command -v kube_ps1 > /dev/null 2>&1; then
-  PS1+=' \[$BLUE\]$(kube_ps1)'
-fi
+  # https://github.com/jonmosco/kube-ps1
+  export KUBE_PS1_SYMBOL_ENABLE=false
+  export KUBE_PS1_PREFIX=''
+  export KUBE_PS1_SUFFIX=''
+  export KUBE_PS1_DIVIDER='/'
+  export KUBE_PS1_CTX_COLOR='magenta'
+  export KUBE_PS1_NS_COLOR='gray'
+  if command -v kube_ps1 > /dev/null 2>&1; then
+    PS1+=' \[$BLUE\]$(kube_ps1)'
+  fi
 
-# Normalize prompt contents
-PS1+='\[${NORMAL}\] $ '
+  PS1+='\[${NORMAL}\]'
+
+  if (( $COLUMNS < 80 )); then
+    PS1+="\n$ "
+  else
+    PS1+=' $ '
+  fi
+}
+
+build_ps1
