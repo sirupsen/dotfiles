f529f5e3d61b2aa87b3d90808476f9ca16ba0103
Simon Eskildsen <sirup@sirupsen.com>
2018-12-01 08:59:57 -0500

    updates

--- home/.bash/shell.bash
+++ home/.bash/shell.bash
@@ -1,15 +1,7 @@
-# Notify immediatly on bg job completion
 set -o notify
-
-# Case insensitive autocompletion
 shopt -s nocaseglob
-
-# Vim as default editorj
 export EDITOR=/usr/local/bin/nvim
-
 export TERM=screen-256color
 export PROMPT_COMMAND="history -a; history -r; ${PROMPT_COMMAND}"
-
-# Plenty big history for searching backwards and doing analysis
 export HISTFILESIZE=100000
 export GPG_TTY=$(tty)
