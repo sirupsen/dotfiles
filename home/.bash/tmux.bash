a59690a52640e98175d8870815fedf3d5f0bf56e
Simon Eskildsen <sirup@sirupsen.com>
2020-04-29 08:56:09 -0400

    review: don't rename tmux when reviewing

--- home/.bash/tmux.bash
+++ home/.bash/tmux.bash
@@ -1,8 +1,10 @@
 rename_tmux_window_to_current_dir() {
   if [[ "$TERM" == "screen-256color" && -n "$TMUX" ]]; then
-    if [[ "$PWD" != "$LPWD" ]]; then
-      LPWD="$PWD"
-      tmux rename-window ${PWD//*\//}
+    if [[ -z "$SKIP_TMUX_RENAME" ]]; then
+      if [[ "$PWD" != "$LPWD" ]]; then
+        LPWD="$PWD"
+        tmux rename-window ${PWD//*\//}
+      fi
     fi
   fi
 }
